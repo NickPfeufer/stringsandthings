36c650a53daf9825b13f3572da0bf45872551323
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2013-09-19 08:20:19 -0600

    I forgot to commit

--- Multi.java
+++ Multi.java
@@ -12,11 +12,50 @@ class Multi {
 		reverseCol(a, 0);
 		int[][] b = {{4, 5},{7,8}};
 		print(b);
-		System.out.println(determinant2x2(b));
+		System.out.println(determinant(b));
+		System.out.println("-----------");
+		print(snakeDraft(10, 4));
+	}
+
+	public static int determinant(int[][] a){
+		int answer = 0;
+		if (a.length == 2) {
+			return determinant2x2(a);
+		}
+		for (int i = 0; i<a.length; i++ ) {
+			
+		}
+		return answer;
+	}
+
+	public static int[] rectangularize(int[] a, int x, int y){
+		int count = 0;
+		int response = [y][x];
+		for (int i = 0; i<y; i++) {
+			for (int j = 0; j < x; j++) {
+				response[i][j] = a[count];
+				count++;
+			}
+		}
+		return response;
 	}
 
 	public static int determinant2x2(int[][] a){
 		return (a[0][0] * a[1][1]) - (a[0][1] * a[1][0]);
+	}
+
+	public static int[][] snakeDraft(int p, int r){
+		int[][] draft = new int[r][p];
+		for (int c = 0; c < r; c++) {
+			for (int row = 0; row < draft[c].length; row++ ) {
+				if (!(c%2==0)) {
+					draft[c][row] = draft[c].length * (c + 1) - row;
+				}else {
+					draft[c][row] = (row+ 1) * (c + 1) + draft[c].length*c ;
+				}
+			}
+		}
+		return draft;
 	}
 
 	public static int[] reverseCol(int[][] a, int c){
