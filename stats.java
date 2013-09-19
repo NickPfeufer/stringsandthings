5172045d25ced863643cb3703af018b28d147c33
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2013-09-19 08:45:13 -0600

    max

---
+++ stats.java
@@ -0,0 +1,17 @@
+class stats{
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public static int max(int[] a){
+		int maximum = a[0];
+		for (int i = 0; i<a.length; i++) {
+			if (maximum < a[i]) {
+				maximum = a[i];
+			}
+		}
+		return maximum;
+	} 
+
+}
